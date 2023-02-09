93b5ddf31fa02a9686ba7393c2d62b15a5bd486a
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-02-09 16:11:28 +0100

    first commit

---
+++ src/currencies.js
@@ -0,0 +1,23 @@
+export const currencies = [
+    {
+        name: "Euro",
+        rate: 4.69,
+        short: "EUR",
+    },
+    {
+        name: "Dolar amerykański",
+        rate: 4.41,
+        short: "USD"
+    },
+    {
+        name: "Frank szwajcarski",
+        rate: 4.74,
+        short: "CHF"
+    },
+    {
+        name: "Funt brytyjski",
+        rate: 4.74,
+        short: "GBP"
+    },
+
+];
