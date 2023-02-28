74465d0fe66d75a459e61e8d82b02057716288db
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-02-28 21:27:02 +0100

    repair result

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,20 +1,20 @@
 import { StyledResult } from "./styled";
 
-export const Result = ({ result }) => {
+export const Result = ({ result }) => (
     <StyledResult>
         {result !== undefined && (
             <>
                 {result.startAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
 
                 <strong>
-                    {result.finalAmount.toFixed(2)}&nbsp;{result.currency}
+                    {result.finalAmount.toFixed(2)} {result.currency}
                 </strong>
             </>
-        )};
+        )}
     </StyledResult>
 
 
-};
+);
 
-export default Result;
+
 
